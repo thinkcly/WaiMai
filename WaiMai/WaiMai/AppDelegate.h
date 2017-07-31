aed114c2b75ffe76191a9fd8e49db45954cf37fe
thinkcly-(SourceTree) <thinkcly@outlook.com (SourceTree)>
2017-07-31 20:11:11 +0800

    v0.01 -> done
    工程创建完成

---
+++ WaiMai/WaiMai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaiMai
+//
+//  Created by Alex Chen on 07/31/2017.
+//  Copyright © 2017 Alex Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaiMai/WaiMai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaiMai
+//
+//  Created by Alex Chen on 07/31/2017.
+//  Copyright © 2017 Alex Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
