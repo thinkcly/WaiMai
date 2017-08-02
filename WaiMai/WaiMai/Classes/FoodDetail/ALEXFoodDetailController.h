c5097b12192afb607d3cf07d194e7406274c6f87
thinkcly-(SourceTree) <thinkcly@outlook.com (SourceTree)>
2017-08-02 20:27:24 +0800

    v0.1.3.1 -> done
    ----------
    与v0.1.3.同理, 更改 ALEXFoodDetail.m 的继承关系, 继承自基类.

--- WaiMai/WaiMai/Classes/FoodDetail/ALEXFoodDetailController.h
+++ WaiMai/WaiMai/Classes/FoodDetail/ALEXFoodDetailController.h
@@ -6,8 +6,9 @@
 //  Copyright © 2017 Alex Inc. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+//#import <UIKit/UIKit.h>     基类ALEXBaseController.h已经继承自UIKit, 所以此处可以省略
+#import "ALEXBaseController.h"
 
-@interface ALEXFoodDetailController : UIViewController
+@interface ALEXFoodDetailController : ALEXBaseController
 
 @end
